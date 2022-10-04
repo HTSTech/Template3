cf186e5ae5cc806c95eef550b67c14e42b8459d5
HTSpankaj <109585437+HTSpankaj@users.noreply.github.com>
2022-10-04 14:29:43 +0530

    Update index.jsx

--- pages/Header/index.jsx
+++ pages/Header/index.jsx
@@ -10,19 +10,19 @@
    
     async function getData() {
       
-      axios.get("https://nqhkpgaezalnpxtizxzv.supabase.co/storage/v1/object/public/projects/7acac802-bed4-45b5-8d75-b2d3786582ec/Template4/Header/index.html").then(res=>{
+      axios.get("https://nqhkpgaezalnpxtizxzv.supabase.co/storage/v1/object/public/projects/7acac802-bed4-45b5-8d75-b2d3786582ec/Template3/Header/index.html").then(res=>{
         console.log("res=> ",res);
         document.getElementById("HeaderID").innerHTML = res.data;
         
         //* css
-        axios.get("https://nqhkpgaezalnpxtizxzv.supabase.co/storage/v1/object/public/projects/7acac802-bed4-45b5-8d75-b2d3786582ec/Template4/Header/style.css").then(CssRes=>{
+        axios.get("https://nqhkpgaezalnpxtizxzv.supabase.co/storage/v1/object/public/projects/7acac802-bed4-45b5-8d75-b2d3786582ec/Template3/Header/style.css").then(CssRes=>{
           console.log("CssRes", CssRes);
           let createCssElement = document.createElement("style");
           // createCssElement.nodeValue = CssRes.data;
           createCssElement.appendChild(document.createTextNode(CssRes.data));
           
           document.getElementById("HeaderID").appendChild(createCssElement)
-          axios.get("https://nqhkpgaezalnpxtizxzv.supabase.co/storage/v1/object/public/projects/7acac802-bed4-45b5-8d75-b2d3786582ec/Template4/Header/script.js").then(JsRes=>{
+          axios.get("https://nqhkpgaezalnpxtizxzv.supabase.co/storage/v1/object/public/projects/7acac802-bed4-45b5-8d75-b2d3786582ec/Template3/Header/script.js").then(JsRes=>{
             console.log("JsRes", JsRes);
             eval(JsRes.data)
           })
